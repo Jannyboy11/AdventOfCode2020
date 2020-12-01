f8f909110671a35cb9ab9962712819a09f16c3a2
Jan Boerman <Jannyboy11@gmail.com>
2020-12-01 14:09:34 +0100

    day1 solutions

--- src/main/scala/com/janboerman/aoc2020/day1/Day1.scala
+++ src/main/scala/com/janboerman/aoc2020/day1/Day1.scala
@@ -1,8 +1,35 @@
 package com.janboerman.aoc2020.day1
 
-@main def main: Unit = {
+import scala.collection.mutable
+import scala.io.Source;
 
-    println("Hello, World!")
+val fileName = "src/main/resources/day1input.txt"
+val numbers = Source.fromFile(fileName).getLines().map(_.toInt).toArray
+
+@main def main: Unit = {
+    
+    var result1 = -1;
+    var result2 = -1
+    
+    for (n <- numbers) {
+        for (o <- numbers) {
+            val sum1 = n + o
+            if (sum1 == 2020) {
+                result1 = n*o
+            }
+            
+            for (p <- numbers) {
+                val sum2 = n + o + p
+                if (sum2 == 2020) {
+                    result2 = n*o*p
+                }
+            }
+
+        }
+    }
+    
+    println(result1)
+    println(result2)
 
 }
 
