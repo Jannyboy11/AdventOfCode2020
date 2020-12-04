6571e90e030cf4505e42dafcb07b78dc9efc063f
Jan Boerman <Jannyboy11@gmail.com>
2020-12-04 08:21:50 +0100

    add day 4 solutions

--- src/main/scala/com/janboerman/aoc2020/day3/Day3.scala
+++ src/main/scala/com/janboerman/aoc2020/day3/Day3.scala
@@ -10,9 +10,10 @@ enum Tile {
 import Tile._
 
 type Grid = IndexedSeq[IndexedSeq[Tile]]
-extension (g: Grid) def apply(x: Int, y: Int): Tile = g(y)(x)
-extension (g: Grid) def width: Int = g(0).size
-extension (g: Grid) def depth: Int = g.size
+extension (g: Grid)
+    def apply(x: Int, y: Int): Tile = g(y)(x)
+    def width: Int = g(0).size
+    def depth: Int = g.size
 
 val source = Source.fromResource("day3input.txt")
 val grid: Grid = source.getLines().map(line => line.map {
@@ -21,10 +22,10 @@ val grid: Grid = source.getLines().map(line => line.map {
 } .toIndexedSeq).toIndexedSeq
 
 @main def main: Unit = {
-    
+
     var result1 = -1;
     var result2 = 1L;
-    
+
     for ((right, down) <- Seq((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))) {
         var treeCount = 0
         var x = 0
@@ -33,20 +34,16 @@ val grid: Grid = source.getLines().map(line => line.map {
             if (grid(x, y) == Tree)
                 treeCount += 1
 
-            x += right
+            x = (x + right) % grid.width
             y += down
-
-            if (x >= grid.width)
-                x -= grid.width
         }
         result2 *= treeCount.toLong
-        
+
         if (right == 3 && down == 1) {
             result1 = treeCount
         }
     }
-    
+
     println(result1)
     println(result2)
 }
-
---
+++ src/main/scala/com/janboerman/aoc2020/day4/Day4.scala
@@ -0,0 +1,117 @@
+package com.janboerman.aoc2020
+package day4
+
+import scala.io.Source;
+import scala.collection.mutable
+
+type Ecl = "ecl";
+type Pid = "pid";
+type Eyr = "eyr";
+type Hcl = "hcl";
+type Byr = "byr";
+type Iyr = "iyr";
+type Cid = "cid";
+type Hgt = "hgt";
+type Key = Ecl | Pid | Eyr | Hcl | Byr | Iyr | Cid | Hgt
+type Value = String
+def the[T](using v: ValueOf[T]): T = v.value
+
+val source = Source.fromResource("day4input.txt")
+val passports = {
+    val list = new mutable.ListBuffer[mutable.Map[Key, Value]]
+    var passport = new mutable.HashMap[Key, Value]()
+    for (line <- source.getLines()) {
+        if (line.isEmpty) {
+            list.append(passport)
+            passport = new mutable.HashMap[Key, Value]();
+        } else {
+            for (keyValuePair <- line.split(" ")) {
+                val Array(k, v) = keyValuePair.split(":")
+                passport.put(k.asInstanceOf[Key], v)
+            }
+        }
+    }
+    list.append(passport)
+    
+    list
+}
+
+def isValid1(passport: mutable.Map[Key, Value]): Boolean = {
+    passport.contains(the[Ecl])
+        && passport.contains(the[Pid])
+        && passport.contains(the[Eyr])
+        && passport.contains(the[Hcl])
+        && passport.contains(the[Byr])
+        && passport.contains(the[Iyr])
+        && passport.contains(the[Hgt])
+}
+
+def isValid2(passport: mutable.Map[Key, Value]): Boolean = {
+    passport.get(the[Byr]) match {
+        case None => return false
+        case Some(byr) =>
+            if (byr.matches("\\d{4}")) {
+                val byrInt = byr.toInt
+                if (byrInt < 1920 || byrInt > 2002) return false
+            } else return false
+    }
+    
+    passport.get(the[Iyr]) match {
+        case None => return false
+        case Some(iyr) =>
+            if (iyr.matches("\\d{4}")) {
+                val iyrInt = iyr.toInt
+                if (iyrInt < 2010 || iyrInt > 2020) return false
+            } else return false
+    }
+
+    passport.get(the[Eyr]) match {
+        case None => return false
+        case Some(eyr) =>
+            if (eyr.matches("\\d{4}")) {
+                val eyrInt = eyr.toInt
+                if (eyrInt < 2020 || eyrInt > 2030) return false
+            } else return false
+    }
+    
+    passport.get(the[Hgt]) match {
+        case None => return false
+        case Some(hgt) =>
+            if (hgt.matches("\\d+cm")) {
+                val cm = hgt.substring(0, hgt.length - 2).toInt
+                if (cm < 150 || cm > 193) return false
+            } else if (hgt.matches("\\d+in")) {
+                val in = hgt.substring(0, hgt.length - 2).toInt
+                if (in < 59 || in > 76) return false
+            } else return false
+    }
+    
+    passport.get(the[Hcl]) match {
+        case None => return false
+        case Some(hcl) =>
+            if (!hcl.matches("#[0-9a-f]{6}")) return false
+    }
+    
+    passport.get(the[Ecl]) match {
+        case None => return false
+        case Some(ecl) =>
+            if (!(ecl == "amb" || ecl == "blu" || ecl == "brn" || ecl == "gry" || ecl == "grn" || ecl == "hzl" || ecl == "oth"))
+                return false
+    }
+    
+    passport.get(the[Pid]) match {
+        case None => return false
+        case Some(pid) =>
+            if (!pid.matches("\\d{9}")) return false
+    }
+    
+    true
+}
+
+@main def main: Unit = {
+    val result1 = passports.count(isValid1)
+    println(result1)
+    
+    val result2 = passports.count(isValid2)
+    println(result2)
+}
